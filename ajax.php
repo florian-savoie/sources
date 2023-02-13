3cc9cd45f43b8bc2da75e2997238fe19f7f82084
florian savoie <117896624+florian-savoie@users.noreply.github.com>
2023-02-13 22:41:01 +0100

    maj

--- ajax.php
+++ ajax.php
@@ -2,30 +2,30 @@
 $titre = "Javascript";
 
  require('inc/head.php');
- $block1 = false ;
- $block2 = false ;
- $block3 = false ;
- $block4 = false ;
- $block5 = false ;
+ $block1 = true ;
+ $block2 = true ;
+ $block3 = true ;
+ $block4 = true ;
+ $block5 = true ;
  $block6 = false ;
 
-$l1 = "floriansavoie/js/tableau-notes/";
-$l2 = "";
-$l3 = "";
-$l4 = "";
-$l5 = "";
+$l1 = "floriansavoie/js/json/jsonv1/json.html";
+$l2 = "floriansavoie/js/json/xmlv1/xml.html";
+$l3 = "floriansavoie/js/json/ajaxbtn/ajax.html";
+$l4 = "floriansavoie/js/json/ajaxget/get.html";
+$l5 = "floriansavoie/js/json/generetableaujs/json2.html";
 $l6 = "";
-$c1 = "tableau notes";
-$c2 = "";
-$c3 = "";
-$c4 = "";
-$c5 = "";
+$c1 = "parcourir un tableau json v1 ";
+$c2 = "manipuler un tableau xml";
+$c3 = "btn affiche text en ajax";
+$c4 = "utiliser ajax requette GET";
+$c5 = "generer tableau JSON grace a JS";
 $c6 = "";
-$img1 = "floriansavoie/js/tableau-notes/assets/img/tableaunotes.png";
-$img2 = "";
-$img3 = "";
-$img4 = "";
-$img5 = "";
+$img1 = "floriansavoie/js/json/assets/img/jsonv1.png";
+$img2 = "floriansavoie/js/json/assets/img/xmlv1.png";
+$img3 = "floriansavoie/js/json/assets/img/ajaxbtnv1.png";
+$img4 = "floriansavoie/js/json/assets/img/ajaxget.png";
+$img5 = "floriansavoie/js/json/assets/img/generertableaujs.png";
 $img6 = "";
 
 
